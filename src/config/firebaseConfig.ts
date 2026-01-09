c586776264a041dd3f34125fb3e2075c325075f5
Douglas Nadoti <douglasnadoti@Douglass-MacBook-Air.local>
2026-01-08 21:51:13 -0300

    retirar codigo

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -2,20 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDtQafuUwKbP27CI1gOgRvXNQ70eOjA3Bo",
-  authDomain: "fiap-extrato.firebaseapp.com",
-  projectId: "fiap-extrato",
-  storageBucket: "fiap-extrato.firebasestorage.app",
-  messagingSenderId: "214305301070",
-  appId: "1:214305301070:web:fed9540041cff87043991f"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: '',
+  appId: ''
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
